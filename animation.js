28767f247a281ea50f3248bc00dcdfb0abd868f2
Dipesh <131427812+jarviskumar@users.noreply.github.com>
2024-05-26 08:17:53 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -2,9 +2,7 @@ new TypeIt("#hTxt", {
     speed: 80,
     waitUntilVisible: true,
   })
-    .type("Dipesh pradhan", { delay: 500 })
-    .move(-6)
-    .delete(1)
-    .type("P")
+    .type("", { delay: 500 })
+    
     .move(null, { to: "END" }, {delay: 400}).go();
   
